5f6f19df6f3ae415b34d57ceca17215dac589d7f
Olena Voroshnina <yuriyilena67@gmail.com>
2022-12-26 12:17:46 +0100

    added the script to the js\01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this link
 
-console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery');
+
+
+
+const makeGalleryItem = ({preview, original, description} = {}) => {
+    return `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+                class="gallery__image"
+                src='${preview}'
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`;
+};
+
+const galleryItemsEl = galleryItems.map( (el) => {
+    return makeGalleryItem(el);
+}).join('');
+
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryItemsEl);
+
+
+galleryListEl.addEventListener('click', onLinkClick);
+
+function onLinkClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const largeImgEl = event.target.getAttribute('data-source');
+
+    const instance = basicLightbox.create(`
+    <img src="${largeImgEl}" width="800" height="600">`)
+
+    instance.show()
+
+}
+
+
